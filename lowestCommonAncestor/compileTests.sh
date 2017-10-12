65d611807a69da0a6cf9b74f852b09b6d8988853
LeonSinclair <lsinclai@tcd.ie>
2017-10-12 16:44:13 +0100

    Fixed compileTests.sh

--- lowestCommonAncestor/compileTests.sh
+++ lowestCommonAncestor/compileTests.sh
@@ -2,5 +2,5 @@
 # This compiles JUnit Tests
 # Takes a single argument of the form packagename/filename.java
 echo $1
-echo javac -d bin -classpath bin:/usr/share/java/junit4.jar -sourcepath src/$1
-javac -d bin -classpath bin:/usr/share/java/junit4.jar -sourcepath src/$1
+echo javac -d bin -classpath bin:/usr/share/java/junit4.jar -sourcepath src $1
+javac -d bin -classpath bin:/usr/share/java/junit4.jar -sourcepath src $1
