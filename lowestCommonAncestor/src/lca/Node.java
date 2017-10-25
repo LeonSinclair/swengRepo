769a666cf1963bacd67f886104378144bcb2d81b
LeonSinclair <lsinclai@tcd.ie>
2017-10-25 21:17:42 +0100

    Revamped Node Class. No longer has parents. Keeps track of indegree and outdegree. List of child nodes now referred to as edgesTo. Disconnect function created. Delete changed to deleteFrom.

--- lowestCommonAncestor/src/lca/Node.java
+++ lowestCommonAncestor/src/lca/Node.java
@@ -2,36 +2,60 @@ package lca;
 import java.util.*;
 public class Node<T>
 {
+     
     public T data;
-    public Node<T> parent;
-    public ArrayList<Node<T>> children;
+    public ArrayList<Node<T>> edgesTo;
     public T key;
+    public int indegree;
+    public int outdegree;
     
     public Node(T keyIn,T value)
     {
         key = keyIn;
         data = value; 
-        parent = null;
-        children = new ArrayList<Node<T>>();       
+        edgesTo = new ArrayList<Node<T>>();
+        indegree = 0;
+        outdegree = edgesTo.size();
     } 
 
-    //makes first node the parent of the second
+    //adds an edge to a child Node
     public void connect(Node<T> child)
     {
         if(child != null)
         {
-            this.children.add(child);
-            child.parent = this;
+            this.edgesTo.add(child);
+            child.indegree++;
         }
         
     }
 
-    public void delete()
+    public void deleteFrom(ArrayList<Node<T>> a)
     {
-        for(int i = 0; i < this.children.size(); i++)
+        if(a==null) return;
+        for(int i = 0; i < a.size(); i++)
         {
-            this.children.get(i).parent = null;
+            Node<T> tmp = a.get(i);
+            if(tmp.edgesTo.contains(this))
+            {
+                tmp.edgesTo.remove(tmp.edgesTo.indexOf(this));
+            }
         }
-        this.children = null;
+        this.edgesTo = null;
     }
+    
+    public void disconnect(Node<T> child)
+    {
+    	if(child!=null)
+    	{
+    		this.edgesTo.remove(child);
+    		child.indegree--;
+    	}
+    }
+    
+    public String toString()
+    {
+    	return this.key.toString();
+    }
+
+
 }
